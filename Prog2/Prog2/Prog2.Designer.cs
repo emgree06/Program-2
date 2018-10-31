a1965511cce1198a9de6a6f99ab5844077dc73b8
emgree06 <44639898+emgree06@users.noreply.github.com>
2018-10-31 08:39:19 -0400

    Add files via upload

---
+++ Prog2/Prog2/Prog2.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Prog2
+{
+    partial class Prog2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.seniorButton = new System.Windows.Forms.RadioButton();
+            this.juniorButton = new System.Windows.Forms.RadioButton();
+            this.sophomoreButton = new System.Windows.Forms.RadioButton();
+            this.freshmanButton = new System.Windows.Forms.RadioButton();
+            this.classStanding = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.regCalculatorButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // seniorButton
+            // 
+            this.seniorButton.AutoSize = true;
+            this.seniorButton.Location = new System.Drawing.Point(100, 52);
+            this.seniorButton.Name = "seniorButton";
+            this.seniorButton.Size = new System.Drawing.Size(55, 17);
+            this.seniorButton.TabIndex = 0;
+            this.seniorButton.TabStop = true;
+            this.seniorButton.Text = "Senior";
+            this.seniorButton.UseVisualStyleBackColor = true;
+            // 
+            // juniorButton
+            // 
+            this.juniorButton.AutoSize = true;
+            this.juniorButton.Location = new System.Drawing.Point(100, 75);
+            this.juniorButton.Name = "juniorButton";
+            this.juniorButton.Size = new System.Drawing.Size(53, 17);
+            this.juniorButton.TabIndex = 1;
+            this.juniorButton.TabStop = true;
+            this.juniorButton.Text = "Junior";
+            this.juniorButton.UseVisualStyleBackColor = true;
+            // 
+            // sophomoreButton
+            // 
+            this.sophomoreButton.AutoSize = true;
+            this.sophomoreButton.Location = new System.Drawing.Point(98, 98);
+            this.sophomoreButton.Name = "sophomoreButton";
+            this.sophomoreButton.Size = new System.Drawing.Size(79, 17);
+            this.sophomoreButton.TabIndex = 2;
+            this.sophomoreButton.TabStop = true;
+            this.sophomoreButton.Text = "Sophomore";
+            this.sophomoreButton.UseVisualStyleBackColor = true;
+            // 
+            // freshmanButton
+            // 
+            this.freshmanButton.AutoSize = true;
+            this.freshmanButton.Location = new System.Drawing.Point(98, 121);
+            this.freshmanButton.Name = "freshmanButton";
+            this.freshmanButton.Size = new System.Drawing.Size(71, 17);
+            this.freshmanButton.TabIndex = 3;
+            this.freshmanButton.TabStop = true;
+            this.freshmanButton.Text = "Freshman";
+            this.freshmanButton.UseVisualStyleBackColor = true;
+            // 
+            // classStanding
+            // 
+            this.classStanding.AutoSize = true;
+            this.classStanding.Location = new System.Drawing.Point(12, 36);
+            this.classStanding.Name = "classStanding";
+            this.classStanding.Size = new System.Drawing.Size(110, 13);
+            this.classStanding.TabIndex = 4;
+            this.classStanding.Text = "Select class standing:";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(12, 153);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(83, 13);
+            this.nameLabel.TabIndex = 5;
+            this.nameLabel.Text = "Enter last name:";
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(98, 169);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(100, 20);
+            this.lastNameText.TabIndex = 6;
+            this.lastNameText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // regCalculatorButton
+            // 
+            this.regCalculatorButton.Location = new System.Drawing.Point(112, 220);
+            this.regCalculatorButton.Name = "regCalculatorButton";
+            this.regCalculatorButton.Size = new System.Drawing.Size(75, 23);
+            this.regCalculatorButton.TabIndex = 7;
+            this.regCalculatorButton.Text = "Calculate";
+            this.regCalculatorButton.UseVisualStyleBackColor = true;
+            this.regCalculatorButton.Click += new System.EventHandler(this.regCalculatorButton_Click);
+            // 
+            // Prog2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.regCalculatorButton);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.classStanding);
+            this.Controls.Add(this.freshmanButton);
+            this.Controls.Add(this.sophomoreButton);
+            this.Controls.Add(this.juniorButton);
+            this.Controls.Add(this.seniorButton);
+            this.Name = "Prog2";
+            this.Text = "Registration Date Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton seniorButton;
+        private System.Windows.Forms.RadioButton juniorButton;
+        private System.Windows.Forms.RadioButton sophomoreButton;
+        private System.Windows.Forms.RadioButton freshmanButton;
+        private System.Windows.Forms.Label classStanding;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Button regCalculatorButton;
+    }
+}
+
---
+++ Prog2/Prog2/Prog2.cs
@@ -0,0 +1,114 @@
+﻿/*
+ * Grading ID: A4431
+ * Progam 2
+ * due date: 10/22/2017
+ * section: 01
+ * */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog2
+{
+    public partial class Prog2 : Form
+    {
+        public Prog2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void regCalculatorButton_Click(object sender, EventArgs e)
+        {
+            string text = lastNameText.Text.ToUpper();// converts the text written in the text box to uppercase
+            string letter;//to be used to extract the first letter
+            char firstLetter;//to be used to turn the first letter into a character
+            string day;//used to display the day in the message box
+            const string SENIOR_DAY = "Nov. 3rd", JUNIOR_DAY = "Nov. 6th", SOPH_DAY1 = "Nov. 7th", SOPH_DAY2 = "Nov. 8th", FRESH_DAY1 = "Nov. 9th", FRESH_DAY2 = "Nov. 10th";// registration days as constants
+            string time;//used to diplay the time in the message box
+            const string TIME1 = "8:30AM", TIME2 = "10:00AM", TIME3 = "11:30AM", TIME4 = "2:00PM", TIME5 = "4:00PM";//registration times as a constant string
+
+            letter = text.Substring(0, 1);//extracts first letter from the last name text box
+            firstLetter = Convert.ToChar(letter);//converts first letter to character data type
+
+
+            if (seniorButton.Checked == true || juniorButton.Checked == true)//this will test to see if the senior or junior radio button is checked
+            {
+                {
+                    if (seniorButton.Checked == true)
+                        day = SENIOR_DAY;
+                    else
+                        day = JUNIOR_DAY;
+                    if (firstLetter <= 'D')
+                        time = TIME2;
+                    else if (firstLetter <= 'I')
+                        time = TIME3;
+                    else if (firstLetter <= 'O')
+                        time = TIME4;
+                    else if (firstLetter <= 'S')
+                        time = TIME5;
+                    else
+                        time = TIME1;
+                }
+                MessageBox.Show($"You can register on {day} at {time}");//displays date and time the student can register based on radio button checked and first letter of the last name
+            }
+            else if (sophomoreButton.Checked == true || freshmanButton.Checked == true)//this will test the sophomore and freshman radio button to determine which is checked
+            {
+                if (sophomoreButton.Checked == true)
+                    /*sophomores and freshman have two days to register depending on the first letter of their last name 
+                     if they have a last name that begins with a letter G-S they will register on Soph_Day2/Fresh_Day2 otherwise it will be Soph_Day1/Fresh_Day1*/
+                {
+                    if (firstLetter >= 'G' && firstLetter <= 'S')
+                        day = SOPH_DAY2;
+                    else
+                        day = SOPH_DAY1;
+                }
+                else
+                {
+                    if (firstLetter >= 'G' && firstLetter <= 'S')
+                        day = FRESH_DAY2;
+                    else
+                        day = FRESH_DAY1;
+                }
+                if (day == SOPH_DAY1 || day == FRESH_DAY1)
+                {
+                    if (firstLetter == 'B' || firstLetter == 'A')
+                        time = TIME3;
+                    else if (firstLetter == 'C' || firstLetter == 'D')
+                        time = TIME4;
+                    else if (firstLetter == 'E' || firstLetter == 'F')
+                        time = TIME5;
+                    else if (firstLetter <= 'V' && firstLetter >= 'T')
+                        time = TIME1;
+                    else
+                        time = TIME2;
+                }
+                else
+                {
+                    if (firstLetter >= 'R')
+                        time = TIME5;
+                    else if (firstLetter >= 'P')
+                        time = TIME4;
+                    else if (firstLetter >= 'M')
+                        time = TIME3;
+                    else if (firstLetter >= 'J')
+                        time = TIME2;
+                    else
+                        time = TIME1;
+                }
+
+                MessageBox.Show($"You can register on {day} at {time}");//Displays the registration date and time based on radio button checked and the first letter of the last name
+            }
+        }
+    }
+}
